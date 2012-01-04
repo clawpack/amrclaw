95bcd1431eec0e4df49a48cb3a655f023f17d267
Randy LeVeque <rjl@uw.edu>
2012-01-04 14:18:13 -0800

    Changed amr_module from f to f90 with implicit none

--- src/2d/amr_module.f90
+++ src/2d/amr_module.f90
@@ -1,173 +1,164 @@
 
        module amr_module
-       implicit double precision(a-h,o-z)
+       implicit none
        
        save
        
-c      ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-c      :::::   data structure info.
-c      ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-c
-       integer    cornxlo,cornylo,cornxhi,cornyhi,timemult
-       integer    store1,store2,storeaux
-       integer    tempptr,errptr,ffluxptr,cfluxptr
-       integer    rsize, horizontal, vertical
+!      ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+!      :::::   data structure info.
+!      ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+!
+       integer, parameter :: rsize = 5
+       integer, parameter :: nsize = 13
 
-       parameter (rsize =  5)
-       parameter (nsize = 13)
+!  :::::::   integer part of node descriptor
+       integer, parameter :: levelptr  = 1
+       integer, parameter :: tempptr   = 2
+       integer, parameter :: errptr    = 3
+       integer, parameter :: nestlevel = 4
+       integer, parameter :: cfluxptr  = 5
+       integer, parameter :: ffluxptr  = 6
+       integer, parameter :: store1    = 7
+       integer, parameter :: store2    = 8
+       integer, parameter :: ndilo     = 9
+       integer, parameter :: ndihi     = 10
+       integer, parameter :: ndjlo     = 11
+       integer, parameter :: ndjhi     = 12
+       integer, parameter :: storeaux  = 13
 
-c  :::::::   integer part of node descriptor
-       parameter (levelptr  = 1)
-       parameter (tempptr   = 2)
-       parameter (errptr    = 3)
-       parameter (nestlevel = 4)
-       parameter (cfluxptr  = 5)
-       parameter (ffluxptr  = 6)
-       parameter (store1    = 7)
-       parameter (store2    = 8)
-       parameter (ndilo     = 9)
-       parameter (ndihi     = 10)
-       parameter (ndjlo     = 11)
-       parameter (ndjhi     = 12)
-       parameter (storeaux  = 13)
+! :::::::  real part of node descriptor
+       integer, parameter :: cornxlo  = 1
+       integer, parameter :: cornylo  = 2
+       integer, parameter :: cornxhi  = 3
+       integer, parameter :: cornyhi  = 4
+       integer, parameter :: timemult = 5
 
-c :::::::  real part of node descriptor
-       parameter (cornxlo  = 1)
-       parameter (cornylo  = 2)
-       parameter (cornxhi  = 3)
-       parameter (cornyhi  = 4)
-       parameter (timemult = 5)
+! :::::::   for linking nodes
+       integer, parameter :: nextfree = 2
+       integer, parameter :: null = 0
+       integer, parameter :: nil  = 0
 
-c :::::::   for linking nodes
-       parameter (nextfree = 2)
-       parameter (null = 0)
-       parameter (nil  = 0)
-
-c :::::::  for flagging points
+! :::::::  for flagging points
        
-       parameter (goodpt = 0.0)
-       parameter (badpt  = 2.0)
-       parameter (badpro = 3.0)
+       real(kind=8), parameter :: goodpt = 0.0
+       real(kind=8), parameter :: badpt  = 2.0
+       real(kind=8), parameter :: badpro = 3.0
 
-       parameter (rinfinity = 10.e32)
-       parameter (iinfinity = 999999)
-       parameter (horizontal = 1)
-       parameter (vertical = 2)
-       parameter  (maxgr = 5000)
-       parameter  (maxlv = 10)
-       parameter  (maxcl = 500)
+       real(kind=8), parameter :: rinfinity = 10.e32
+       integer, parameter :: iinfinity = 999999
+       integer, parameter :: horizontal = 1
+       integer, parameter :: vertical = 2
+       integer, parameter :: maxgr = 5000
+       integer, parameter :: maxlv = 10
+       integer, parameter :: maxcl = 500
 
-c      The max1d parameter should be changed if using OpenMP grid based 
-c      looping, usually set to max1d = 60
-       parameter  (max1d = 60)
+!      The max1d parameter should be changed if using OpenMP grid based 
+!      looping, usually set to max1d = 60
+       integer, parameter :: max1d = 60
 
-       parameter  (maxvar = 10)
-       parameter  (maxaux = 20)
-       parameter  (maxout = 5000)
+       integer, parameter :: maxvar = 10
+       integer, parameter :: maxaux = 20
+       integer, parameter :: maxout = 5000
 
        logical    printout,matlabout,ncarout
 
-       double precision hxposs(maxlv), hyposs(maxlv),possk(maxlv),
-     &         rnode(rsize, maxgr) 
+       real(kind=8) hxposs(maxlv), hyposs(maxlv),possk(maxlv),&
+               rnode(rsize, maxgr) 
 
 
 
-       double precision tol, tolsp
-       integer ibuff,  mstart, ndfree, lfine, node(nsize, maxgr),
-     &         icheck(maxlv),lstart(maxlv),newstl(maxlv),
-     &         listsp(maxlv),intratx(maxlv),intraty(maxlv),
-     &         kratio(maxlv), iregsz(maxlv),jregsz(maxlv),
-     &         iregst(maxlv),jregst(maxlv),
-     &         iregend(maxlv),jregend(maxlv),
-     &         numgrids(maxlv),numcells(maxlv),
-     &         iorder,mxnest,kcheck,nghost
+       real(kind=8) tol, tolsp
+       integer ibuff,  mstart, ndfree, lfine, node(nsize, maxgr), &
+               icheck(maxlv),lstart(maxlv),newstl(maxlv), &
+               listsp(maxlv),intratx(maxlv),intraty(maxlv), &
+               kratio(maxlv), iregsz(maxlv),jregsz(maxlv), &
+               iregst(maxlv),jregst(maxlv), &
+               iregend(maxlv),jregend(maxlv), &
+               numgrids(maxlv),numcells(maxlv), &
+               iorder,mxnest,kcheck,nghost
 
        integer matlabu,ngrids
-c
-c      ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-c      ::::  for alloc array/memory
-c      ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+!
+!      ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+!      ::::  for alloc array/memory
+!      ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-c       Static memory implementation
-c        parameter  (memsize = 10000000)
-c        common  /calloc/   alloc(memsize)
+!       Static memory implementation
+!        parameter  (memsize = 10000000)
+!        common  /calloc/   alloc(memsize)
 
-c      Dynamic memory: 
-       double precision, allocatable, target, dimension(:) ::
-     &        storage
-       double precision, pointer, dimension(:) :: alloc
+!      Dynamic memory: 
+       real(kind=8), allocatable, target, dimension(:) :: storage
+       real(kind=8), pointer, dimension(:) :: alloc
        integer memsize
        
        
-c
-c      ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-c      :::::   for space management of alloc array
-c      ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-c
-       parameter (lfdim=5000)
+!
+!      ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+!      :::::   for space management of alloc array
+!      ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+!
+       integer, parameter :: lfdim=5000
 
        integer lfree(lfdim,2),lenf
 
-c
-c      ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-c      :::::  domain description variables
-c      ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-c
+!
+!      ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+!      :::::  domain description variables
+!      ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+!
        logical xperdom, yperdom, spheredom
-       double precision xupper,yupper,xlower,ylower
+       real(kind=8) xupper,yupper,xlower,ylower
 
-c
-c      ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-c      :::::  collect stats
-c      ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-c
-       double precision rvoll(10),evol,rvol
+!
+!      ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+!      :::::  collect stats
+!      ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+!
+       real(kind=8) rvoll(10),evol,rvol
        integer lentot,lenmax,lendim
 
-c
-c      ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-c      :::::  method parameters
-c      ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-c
-       parameter (maxwave = 10)
+!
+!      ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+!      :::::  method parameters
+!      ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+!
+       integer, parameter :: maxwave = 10
        character * 10 auxtype(maxaux)
        integer  method(7), mthlim(maxwave), mwaves, mcapa
-       double precision cfl,cflmax,cflv1,cfl_level
-c
-c      ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-c      ::::  for i/o assignments
-c      ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-c
+       real(kind=8) cfl,cflmax,cflv1,cfl_level
+!
+!      ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+!      ::::  for i/o assignments
+!      ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+!
 
-       integer chkunit,dbugunit,inunit,outunit,pltunit1,rstunit
-       integer matunit,parmunit
-
-       parameter (parmunit = 12)
-       parameter (chkunit = 10)
-       parameter (inunit  = 5)
-       parameter (outunit = 66)
-       parameter (pltunit1 = 3)
-       parameter (rstunit = 9)
-       parameter (dbugunit = 11)
-       parameter (matunit = 70)
+       integer, parameter :: parmunit = 12
+       integer, parameter :: chkunit = 10
+       integer, parameter :: inunit  = 5
+       integer, parameter :: outunit = 66
+       integer, parameter :: pltunit1 = 3
+       integer, parameter :: rstunit = 9
+       integer, parameter :: dbugunit = 11
+       integer, parameter :: matunit = 70
 
 
-c      ::::  common for debugging flags (verbose output)
+!      ::::  common for debugging flags (verbose output)
 
-       logical
-     &         dprint,     !  domain flags output
-     &         eprint,     !  error estimation output
-     &         edebug,     !  even more error estimation output
-     &         gprint,     !  verbose grid generation (clustering,colating...)
-     &         nprint,     !  nestck reporting
-     &         pprint,     !  projec tagged pts.
-     &         rprint,     !  regridding -  summary of new grids
-     &         sprint,     !  space (memory) output
-     &         tprint,     !  tick (time stepping) reporting
-     &         uprint      !  updating/upbnding reporting
+       logical &
+               dprint,     & !  domain flags output
+               eprint,     & !  error estimation output
+               edebug,     & !  even more error estimation output
+               gprint,     & !  verbose grid generation (clustering,colating...)
+               nprint,     & !  nestck reporting
+               pprint,     & !  projec tagged pts.
+               rprint,     & !  regridding -  summary of new grids
+               sprint,     & !  space (memory) output
+               tprint,     & !  tick (time stepping) reporting
+               uprint        !  updating/upbnding reporting
 
 
-c      variables for conservation checking:
-       double precision tstart,tmass0
+!      variables for conservation checking:
+       real(kind=8) tstart,tmass0
 
        end module amr_module
