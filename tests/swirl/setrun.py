9a3035b745811798cd39edee36940f9a11f052ba
Randy LeVeque <rjl@ned>
2012-10-07 19:54:01 -0400

    Fixed tests/swirl example and added gauges to test

--- tests/swirl/setrun.py
+++ tests/swirl/setrun.py
@@ -108,24 +108,24 @@ def setrun(claw_pkg='amrclaw'):
     # Specify at what times the results should be written to fort.q files.
     # Note that the time integration stops after the final output time.
  
-    clawdata.output_style = 3
+    clawdata.output_style = 1
  
     if clawdata.output_style==1:
         # Output ntimes frames at equally spaced times up to tfinal:
         # Can specify num_output_times = 0 for no output
-        clawdata.num_output_times = 2
-        clawdata.tfinal = 0.500000
+        clawdata.num_output_times = 25
+        clawdata.tfinal = 2.5
         clawdata.output_t0 = True  # output at initial (or restart) time?
         
     elif clawdata.output_style == 2:
         # Specify a list or numpy array of output times:
         # Include t0 if you want output at the initial time.
-        clawdata.output_times =  [0., 0.1]
+        clawdata.output_times =  [0., 0.5, 1.0]
  
     elif clawdata.output_style == 3:
         # Output every step_interval timesteps over total_steps timesteps:
-        clawdata.output_step_interval = 2
-        clawdata.total_steps = 4
+        clawdata.output_step_interval = 1
+        clawdata.total_steps = 10
         clawdata.output_t0 = True  # output at initial (or restart) time?
         
 
@@ -229,6 +229,14 @@ def setrun(claw_pkg='amrclaw'):
     clawdata.bc_lower[1] = 'periodic'   # at ylower
     clawdata.bc_upper[1] = 'periodic'   # at yupper
                          
+
+    # ---------------
+    # Gauges:
+    # ---------------
+    clawdata.gauges = []
+    # for gauges append lines of the form  [gaugeno, x, y, t1, t2]
+    clawdata.gauges.append([1, 0.6, 0.6, 0, 1e9])
+
     
 
     # ---------------
@@ -277,12 +285,17 @@ def setrun(claw_pkg='amrclaw'):
     clawdata.verbosity_regrid = 0      
 
 
-    # == setgauges.data values ==
-    clawdata.gauges = []
-    # for gauges append lines of the form  [gaugeno, x, y, t1, t2]
-    clawdata.gauges.append([1, 0.5, 0.6, 0, 1e9])
-    clawdata.gauges.append([2, 0.9, 0.9, 0, 1e9])
+    # ---------------
+    # Regions:
+    # ---------------
+    clawdata.regions = []
+    # to specify regions of refinement append lines of the form
+    #  [minlevel,maxlevel,t1,t2,x1,x2,y1,y2]
 
+
+    # --------------
+    # Checkpointing:
+    # --------------
 
     # Specify when checkpoint files should be created that can be
     # used to restart a computation.
