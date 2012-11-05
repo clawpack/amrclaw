f21a7dfd8b349baadf70de5df0c342492dbbfc2d
Kyle Mandli <kyle.mandli@gmail.com>
2012-11-05 11:55:33 -0600

    Add new routines to all Makefiles

--- tests/shockbubble/setrun.py
+++ tests/shockbubble/setrun.py
@@ -146,7 +146,7 @@ def setrun(claw_pkg='amrclaw'):
     # The current t, dt, and cfl will be printed every time step
     # at AMR levels <= verbosity.  Set verbosity = 0 for no printing.
     #   (E.g. verbosity == 2 means print only on levels 1 and 2.)
-    clawdata.verbosity = 0
+    clawdata.verbosity = 1
     
     
 
