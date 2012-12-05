2d0af0cd4bb0ba375ae00b461a67467f47ab4d98
Randy LeVeque <rjl@ned>
2012-12-05 08:09:18 -0500

    comment out regions in swirl/setrun.py

--- tests/swirl/setrun.py
+++ tests/swirl/setrun.py
@@ -288,8 +288,8 @@ def setrun(claw_pkg='amrclaw'):
     rundata.regiondata.regions = []
     # to specify regions of refinement append lines of the form
     #  [minlevel,maxlevel,t1,t2,x1,x2,y1,y2]
-    rundata.regiondata.regions.append([3, 3, 0.0, 0.3, 0.2, 0.3, 0.2, 0.3])
-    rundata.regiondata.regions.append([3, 3, 1.8, 2.5, 0.2, 0.3, 0.2, 0.3])
+    #rundata.regiondata.regions.append([3, 3, 0.0, 0.3, 0.2, 0.3, 0.2, 0.3])
+    #rundata.regiondata.regions.append([3, 3, 1.8, 2.5, 0.2, 0.3, 0.2, 0.3])
 
 
     # --------------
