98fce8df7e32f4673420ddbc8cca6484d28e640f
Marsha Berger <berger@cims.nyu.edu>
2013-07-22 15:34:38 -0700

    improved by Donna

--- tests/acoustic3d/interface/afterframe.m
+++ tests/acoustic3d/interface/afterframe.m
@@ -7,3 +7,20 @@ hold on;
 plot3([-1 1],[0 0],[0 0],'k--')
 plot3([0 0],[-1 1],[0 0],'k--')
 hold off
+
+showpatchborders;
+setpatchborderprops(1,'linewidth',2,'color','k');  % new version only
+setpatchborderprops(2,'linewidth',2,'color','b');  % new version only
+% setpatchbordercolor('r',1);
+% setpatchbordercolor('b',2);
+
+showcubes;
+setcubecolor('k',1);
+setcubecolor('b',2);
+
+
+colorbar;
+
+shg;
+
+clear afterframe;
