7cf70c8be83b86e37f2eebde3fd66df21a0b3f43
Marsha Berger <berger@cims.nyu.edu>
2013-08-08 14:37:28 -0700

    more module files and now f90 not f files

---
+++ src/3d/init_alloc.f90
@@ -0,0 +1,26 @@
+! ============================================================================
+!  Program:     AMRClaw
+!  File:        init_alloc.f90
+!  Created:     2009-01-21
+!  Author:      Kyle Mandli and Marsha Berger
+! ============================================================================
+!  Description:  Initialization of alloc storage
+! ============================================================================
+
+
+      subroutine init_alloc()
+    
+      use amr_module
+      implicit none
+
+       if (.not.allocated(storage)) then
+          memsize = 2000000
+          allocate(storage(memsize))
+          alloc => storage
+          print *, "Storage allocated..."
+      else
+          print *, "Storage already allocated!"
+      endif
+    
+      end subroutine init_alloc
+
