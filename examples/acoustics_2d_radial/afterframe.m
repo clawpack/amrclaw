bdcdad1d7707808a0333dfb69b28e86ef8ee406d
Randy LeVeque <rjl@ned>
2013-09-14 10:50:42 -0700

    fixed examples/acoustics_2d_radial to add .m files from @donnaaboise PR #65 and fix 1drad subdirectory to properly use new classic code

---
+++ examples/acoustics_2d_radial/afterframe.m
@@ -0,0 +1,36 @@
+if (PlotType == 1)
+  daspect([1 1 1]);
+  rybcolormap;
+  showgridlines(1:2);
+  showpatchborders;
+
+  caxis([-2 2]);
+  colorbar;
+elseif (PlotType == 4)
+  hold on;
+  dir = './1drad/_output/';
+  dim = 1;
+  [amrdata1d,t1d] = readamrdata(dim,Frame,dir);
+  if (abs(t1d - t) > 1e-5)
+    error('afterframe : 1d reference solution is not time synchronized');
+  end;
+  [q1d,x1d,p] = plotframe1ez(amrdata1d,mq,'b-');
+  set(p,'linewidth',2);
+
+  ph = getlegendinfo;
+  lh = legend([ph,p],{'level 1','level 2','level 3','Exact'});
+  set(lh,'fontsize',16);
+
+  hold off;
+end
+
+
+prt = false;
+if (prt)
+  fname = framename(Frame,'radial0000','png');
+  print('-dpng',fname);
+end
+
+
+
+clear afterframe;
