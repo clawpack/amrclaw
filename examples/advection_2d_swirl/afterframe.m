467daf521c7fdb4559c8cb9d9c885687246419c0
Donna Calhoun <donna.calhoun@gmail.com>
2013-10-06 13:06:40 -0600

    Show under/over shoots in swirl example

--- examples/advection_2d_swirl/afterframe.m
+++ examples/advection_2d_swirl/afterframe.m
@@ -1,6 +1,7 @@
 axis([0 1 0 1]);
 daspect([1 1 1]);
 
+% Colorbar is set in 'underover';
 % yrbcolormap;
 
 showpatchborders;
@@ -8,15 +9,21 @@ showpatchborders;
 hidegridlines();
 showpatchborders();
 hidecontourlines;
-setpatchborderprops(1:5,'linewidth',2);
-showgridlines(5);
+showgridlines(1:2);
 
 caxis([0 1]);
 
 fprintf('%10s : %24.16f\n','qmin',qmin);
 fprintf('%10s : %24.16f\n','qmax',qmax);
 
-underover_colorbar;
+qlo = 0;
+qhi = 1;
+under_label = sprintf('%3.1f - %7.1e',qlo,qlo-qmin);
+over_label = sprintf('%3.1f + %7.1e',qhi,qmax-qhi);
+colorbar_underover(under_label,over_label);
+ 
 
+shg;
 
+clear underover;
 clear afterframe;
--- examples/advection_2d_swirl/underover.m
+++ examples/advection_2d_swirl/underover.m
@@ -1,52 +1,17 @@
 function uo = underover()
-
-% UNDEROVER returns structure for assigning colors to under/overshoots
-%
-% UO = UNDEROVER().  The user needs to define the fields as follows
-%
-%          color_under     the rgb triple used to color undershoots
-%          color_over      the rgb triple used to color overshoots
-%          value_lower     lower value of exact solution
-%          value_upper     upper value of exact solution
-%          tol             tolerance for under/over shoots
-%          colormap        colormap for values in [qlow-tol,qhi+tol];
-%
-% Values are considered in the overshoot/undershoot region if they satisfy
-%
-%                 q < value_lower - tol
-% or
-%                 q > value_upper + tol
-%
-% Values not in the under/overshoot region are mapped into the colormap
-% according to
-%
-%                    slope = (q-qlow)/(qhi-qlow)
-%
-%                    idx = 1 + slope*(nmax-1)
-%
-% An extended colormap is created as :
-%
-%                extended_colormap = [color_under; colormap; color_over];
-%
-% Using the extended colormap, the 'cdata' property of the finite volume
-% patch is set to an rgb triple.  Values in the overshoot region are set to
-% color_over, values in the undershoot region are set to color_under, values
-% in [value_lower-tol, value_lower] are set to the first color in
-% 'colormap',  values in [value_upper, value_upper+tol] are set to the last
-% values in 'colormap', and values in [value_lower, value_upper] are
-% linearly scaled into the colormap.
-%
-% This function is called from SETCOLORS
-%
-% See also SETCOLORS
+%UNDEROVER returns structure for assigning colors to under/overshoots
+% 
+%   For complete help, type 
+% 
+%   >> help visclaw/src/matlab/underover
 %
 
 yrbcolormap;
-cm = colormap(jet(64));
+cm = colormap;
 
 uo = struct('color_under',[0 1 1],...
-            'color_over',[1 0 1],...
-            'value_lower',0, ...
-	    'value_upper',1,...
-	    'tol',1e-4,...
-	    'colormap',cm);
+    'color_over',[1 0 1],...
+    'value_lower',0, ...
+    'value_upper',1,...
+    'tol',1e-6,...
+    'colormap',cm);
