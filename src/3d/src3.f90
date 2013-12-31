5c0abd7cffe6168ef75f435e715adb017e7b76cc
Randy LeVeque <rjl@ned>
2013-12-30 16:51:31 -0800

    changed double precision to real(kind=8)

--- src/3d/src3.f90
+++ src/3d/src3.f90
@@ -8,7 +8,7 @@ subroutine src3(meqn,mbc,mx,my,mz,xlower,ylower,zlower,dx,dy,dz,q,maux,aux,t,dt)
     implicit none
     integer, intent(in) :: meqn,mbc,mx,my,mz,maux
     real(kind=8), intent(in) :: xlower,ylower,zlower,dx,dy,dz,t,dt
-    double precision, intent(in) :: aux(maux,1-mbc:mx+mbc,1-mbc:my+mbc)
-    double precision, intent(inout) :: q(meqn,1-mbc:mx+mbc,1-mbc:my+mbc)
+    real(kind=8), intent(in) :: aux(maux,1-mbc:mx+mbc,1-mbc:my+mbc)
+    real(kind=8), intent(inout) :: q(meqn,1-mbc:mx+mbc,1-mbc:my+mbc)
 
 end subroutine src3
