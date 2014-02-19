71881a6691cd1aac29a2e63f3f17e6ffd0eac698
Randy LeVeque <rjl@ned>
2014-02-19 10:11:57 -0800

    added my_env to run_tests.py

--- examples/run_tests.py
+++ examples/run_tests.py
@@ -5,9 +5,12 @@ Run all examples and then compare to gallery versions, if available.
 from clawpack.clawutil import regression_tests, make_all
 import os
 
-os.environ['GIT_STATUS'] = 'True'
+my_env = os.environ
+my_env['GIT_STATUS'] = 'True'
+my_env['FFLAGS'] = '-O2 -fopenmp'
+my_env['OMP_NUM_THREADS'] = '3'
 
-make_all.make_all(make_clean_first=True)
+make_all.make_all(make_clean_first=True, env=my_env)
 
 print "\n-----------------------------------------------------------\n"
 
