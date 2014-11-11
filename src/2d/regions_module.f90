24ecc709f0e71e81462fd2355a47637c28083364
David George <dave.jorge@gmail.com>
2014-11-11 06:14:46 -0800

    modified regions module to allow arbitrary polygons

--- src/2d/regions_module.f90
+++ src/2d/regions_module.f90
@@ -14,12 +14,28 @@ module regions_module
         real(kind=8) :: x_low,y_low,x_hi,y_hi,t_low,t_hi
     end type region_type
 
+    ! poly Region type definition
+    type polyregion_type
+        integer :: min_level,max_level
+        integer :: num_points
+        real(kind=8) :: t_low,t_hi
+        real(kind=8), allocatable :: xpoints(:)
+        real(kind=8), allocatable :: ypoints(:)
+
+    end type polyregion_type
+
     integer :: num_regions
     type(region_type), allocatable :: regions(:)
+
+
+    integer :: num_polyregions
+    type(polyregion_type), allocatable :: polyregions(:)
       
 contains
 
+
     subroutine set_regions(fname)
+        ! this sets arbitrary polygon regions
 
         use amr_module
       
@@ -30,11 +46,11 @@ contains
       
         ! Locals
         integer, parameter :: unit = 7
-        integer :: i
+        integer :: i,ip,np
 
         write(parmunit,*) ' '
         write(parmunit,*) '--------------------------------------------'
-        write(parmunit,*) 'REGIONS:'
+        write(parmunit,*) 'POLY REGIONS:'
         write(parmunit,*) '-----------'
 
         if (present(fname)) then
@@ -43,22 +59,30 @@ contains
             call opendatafile(unit,'regions.data')
         endif
 
-        read(unit,"(i2)") num_regions
-        if (num_regions == 0) then
-            write(parmunit,*) '  No regions specified for refinement'
+        read(unit,"(i2)") num_polyregions
+        if (num_polyregions == 0) then
+            write(parmunit,*) '  No polyregions specified for refinement'
             
         else
-            ! Refinement region data
-            allocate(regions(num_regions))
-            do i=1,num_regions
-                read(unit,*) regions(i)%min_level, regions(i)%max_level, &
-                             regions(i)%t_low, regions(i)%t_hi, &
-                             regions(i)%x_low, regions(i)%x_hi, &
-                             regions(i)%y_low, regions(i)%y_hi
+            ! Refinement poly region data
+            allocate(polyregions(num_polyregions))
+            do i=1,num_polyregions
+                read(unit,*) polyregions(i)%num_points, &
+                             polyregions(i)%min_level, polyregions(i)%max_level, &
+                             polyregions(i)%t_low, polyregions(i)%t_hi
+
+                np = polyregions(i)%num_points
+                allocate(polyregions(i)%xpoints(np))
+                allocate(polyregions(i)%ypoints(np))
+                do ip = 1,np
+                    read(unit,*) polyregions(i)%xpoints(ip), &
+                                 polyregions(i)%ypoints(ip) 
+                enddo
             enddo
         endif
         close(unit)
 
     end subroutine set_regions
+
 
 end module regions_module
