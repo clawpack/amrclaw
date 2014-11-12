188977fc244e2a5f32dc7c36195dd17eb71e0c94
David George <dave.jorge@gmail.com>
2014-11-12 10:52:08 -0800

    implemented polygon regions. Note, python must still be altered to create proper regions.data from setrun.py

--- src/2d/regions_module.f90
+++ src/2d/regions_module.f90
@@ -8,28 +8,20 @@ module regions_module
     implicit none
     save
 
-    ! Region type definition
-    type region_type
-        integer :: min_level,max_level
-        real(kind=8) :: x_low,y_low,x_hi,y_hi,t_low,t_hi
-    end type region_type
-
     ! poly Region type definition
     type polyregion_type
         integer :: min_level,max_level
         integer :: num_points
         real(kind=8) :: t_low,t_hi
+        real(kind=8) :: x_low,x_hi,y_low,y_hi
         real(kind=8), allocatable :: xpoints(:)
         real(kind=8), allocatable :: ypoints(:)
 
     end type polyregion_type
 
+
     integer :: num_regions
-    type(region_type), allocatable :: regions(:)
-
-
-    integer :: num_polyregions
-    type(polyregion_type), allocatable :: polyregions(:)
+    type(polyregion_type), allocatable :: regions(:)
       
 contains
 
@@ -50,7 +42,7 @@ contains
 
         write(parmunit,*) ' '
         write(parmunit,*) '--------------------------------------------'
-        write(parmunit,*) 'POLY REGIONS:'
+        write(parmunit,*) 'REGIONS:'
         write(parmunit,*) '-----------'
 
         if (present(fname)) then
@@ -59,25 +51,32 @@ contains
             call opendatafile(unit,'regions.data')
         endif
 
-        read(unit,"(i2)") num_polyregions
-        if (num_polyregions == 0) then
-            write(parmunit,*) '  No polyregions specified for refinement'
+        read(unit,"(i2)") num_regions
+        if (num_regions == 0) then
+            write(parmunit,*) '  No regions specified for refinement'
             
         else
             ! Refinement poly region data
-            allocate(polyregions(num_polyregions))
-            do i=1,num_polyregions
-                read(unit,*) polyregions(i)%num_points, &
-                             polyregions(i)%min_level, polyregions(i)%max_level, &
-                             polyregions(i)%t_low, polyregions(i)%t_hi
+            allocate(regions(num_regions))
+            do i=1,num_regions
+                read(unit,*) regions(i)%num_points, &
+                             regions(i)%min_level, regions(i)%max_level, &
+                             regions(i)%t_low, regions(i)%t_hi
 
-                np = polyregions(i)%num_points
-                allocate(polyregions(i)%xpoints(np))
-                allocate(polyregions(i)%ypoints(np))
+                np = regions(i)%num_points
+                allocate(regions(i)%xpoints(np+1))
+                allocate(regions(i)%ypoints(np+1))
+
                 do ip = 1,np
-                    read(unit,*) polyregions(i)%xpoints(ip), &
-                                 polyregions(i)%ypoints(ip) 
+                    read(unit,*) regions(i)%xpoints(ip), &
+                                 regions(i)%ypoints(ip) 
                 enddo
+                regions(i)%xpoints(np+1) = regions(i)%xpoints(1) 
+                regions(i)%ypoints(np+1) = regions(i)%ypoints(1) 
+                regions(i)%x_low = minval(regions(i)%xpoints)
+                regions(i)%y_low = minval(regions(i)%ypoints)
+                regions(i)%x_hi =  maxval(regions(i)%xpoints)
+                regions(i)%y_hi =  maxval(regions(i)%ypoints)
             enddo
         endif
         close(unit)
