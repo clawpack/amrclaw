8e5369dfb2459581c95e481b1fd81c362f174ceb
Marsha Berger <berger@cims.nyu.edu>
2015-01-10 20:54:29 -0500

    updated dyamic storage to use allocatable arrays instead of pointers

--- src/3d/init_alloc.f90
+++ src/3d/init_alloc.f90
@@ -13,10 +13,12 @@
       use amr_module
       implicit none
 
-       if (.not.allocated(storage)) then
+!      if (.not.allocated(storage)) then   ! old way, changed mjb jan. 2015
+       if (.not.allocated(alloc)) then     ! new way, use allocatable arrays, not pointers
           memsize = 2000000
-          allocate(storage(memsize))
-          alloc => storage
+!         allocate(storage(memsize))
+          allocate(alloc(memsize))
+!          alloc => storage
           print *, "Storage allocated..."
       else
           print *, "Storage already allocated!"
