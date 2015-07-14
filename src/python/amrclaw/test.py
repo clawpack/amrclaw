b0091cded5a4db8f24717234a11a8dbe33a8ec1d
Kyle Mandli <kyle.mandli@gmail.com>
2015-07-14 15:21:38 -0400

    Add new test framework

---
+++ src/python/amrclaw/test.py
@@ -0,0 +1,36 @@
+r"""
+Execute nosetests in all subdirectories, to run a series of quick
+regression tests.
+
+Sends output and result/errors to separate files to simplify checking
+results and looking for errors.
+"""
+
+import os
+import glob
+
+import clawpack.clawutil.test
+import clawpack.pyclaw.util
+
+# Clean library files whenever this module is used
+if os.environ.has_key("CLAW"):
+    CLAW = os.environ["CLAW"]
+else:
+    raise ValueError("Need to set CLAW environment variable.")
+
+for lib_path in [os.path.join(CLAW,"amrclaw","src","2d"),
+                 os.path.join(CLAW,"amrclaw","src","3d")]:
+    for path in glob.glob(os.path.join(lib_path,"*.o")):
+        os.remove(path)
+    for path in glob.glob(os.path.join(lib_path,"*.mod")):
+        os.remove(path)
+
+
+class AMRClawRegressionTest(clawpack.clawutil.test.ClawpackRegressionTest):
+
+    r"""Base AMRClaw regression test setup derived from ClawpackRegressionTest
+
+    """
+
+    __doc__ += clawpack.pyclaw.util.add_parent_doc(
+                                  clawpack.clawutil.test.ClawpackRegressionTest)
