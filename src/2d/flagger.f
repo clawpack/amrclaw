1371076e1252f7b04a778c0fa62b46e723e3fcca
Marsha Berger <berger@cims.nyu.edu>
2015-09-04 11:28:42 -0400

    substituted one more place for array of grids  traversal

--- src/2d/flagger.f
+++ src/2d/flagger.f
@@ -20,11 +20,13 @@ c the two approaches share an array with boundary ghost values
 c
 c ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-      call prepgrids(listgrids,numgrids(lcheck),lcheck)
+c      call prepgrids(listgrids,numgrids(lcheck),lcheck)
          mbuff = max(nghost,ibuff+1)  
 c before parallel loop give grids the extra storage they need for error estimation
          do  jg = 1, numgrids(lcheck)
-            mptr = listgrids(jg)
+c            mptr = listgrids(jg)
+            levSt  = listStart(lcheck)
+            mptr   = listOfgrids(levSt+jg-1)
             nx     = node(ndihi,mptr) - node(ndilo,mptr) + 1
             ny     = node(ndjhi,mptr) - node(ndjlo,mptr) + 1
             mitot  = nx + 2*nghost
@@ -51,7 +53,9 @@ c before parallel loop give grids the extra storage they need for error estimati
 !$OMP&            DEFAULT(none),
 !$OMP&            SCHEDULE(DYNAMIC,1)
        do  jg = 1, numgrids(lcheck)
-          mptr = listgrids(jg)
+c          mptr = listgrids(jg)
+          levSt  = listStart(lcheck)
+          mptr   = listOfgrids(levSt+jg-1)
           nx     = node(ndihi,mptr) - node(ndilo,mptr) + 1
           ny     = node(ndjhi,mptr) - node(ndjlo,mptr) + 1
           mitot  = nx + 2*nghost
