b3372237436e45d8d39817f1bc2b07b8cf890f3d
Marsha Berger <berger@cims.nyu.edu>
2016-02-09 11:39:51 -0500

    added comment about unit numbers >=89 reserved for gauges

--- src/2d/amr_module.f90
+++ src/2d/amr_module.f90
@@ -61,7 +61,8 @@ module amr_module
 
     ! The max1d parameter should be changed if using OpenMP grid based 
     ! looping, usually set to max1d = 60
-    integer, parameter :: max1d = 60 
+    integer, parameter :: max1d = 30 
+    !integer, parameter :: max1d = 60 
     !integer, parameter :: max1d = 100 
     !integer, parameter :: max1d = 80 
     !integer, parameter :: max1d = 500 
@@ -168,6 +169,8 @@ module amr_module
 
     integer :: matlabu
 
+    !  USE UNITS NUMBERS < 89.
+    ! 89 and + numthreads taken by gauge output
     integer, parameter :: parmunit = 12
     integer, parameter :: chkunit = 10
     integer, parameter :: inunit  = 5
