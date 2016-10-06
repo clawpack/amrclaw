14c76e6816df7dbd08c50c69d85db7171bc7836a
Brisa Davis <bndavis@uw.edu>
2016-10-06 11:25:34 -0700

    Adding init_alloc.f90 for 1d code.

---
+++ src/1d/init_alloc.f90
@@ -0,0 +1,28 @@
+! ============================================================================
+!  Program:     AMRClaw
+!  File:        init_alloc.f90
+!  Created:     2009-01-21
+!  Author:      Kyle Mandli and Marsha Berger
+! ============================================================================
+!  Description:  Initialization of alloc storage
+! ============================================================================
+
+
+subroutine init_alloc()
+    
+    use amr_module
+    implicit none
+    
+!    if (.not.allocated(storage)) then   ! old way, changed mjb sept. 2014
+    if (.not.allocated(alloc)) then      ! new way, use allocatable arrays, not pointers
+        memsize = 1000000
+!        allocate(storage(memsize)) 
+        allocate(alloc(memsize))
+!        alloc => storage
+        print *, "Storage allocated..."
+    else
+        print *, "Storage already allocated!"
+    endif
+    
+end subroutine init_alloc
+
