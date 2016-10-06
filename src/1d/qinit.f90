f5e521029d5c59791a226ff23e6a6a30684737b6
Brisa Davis <bndavis@uw.edu>
2016-10-06 14:20:54 -0700

    Adding actual qinit.f90 for 1d code.

---
+++ src/1d/qinit.f90
@@ -0,0 +1,19 @@
+! Default qinit file
+subroutine qinit(meqn,mbc,mx,xlower,dx,q,maux,aux)
+
+    ! Set initial conditions for the q array.
+    ! This default version prints an error message since it should
+    ! not be used directly.  Copy this to an application directory and
+    ! loop over all grid cells to set values of q(1:meqn, 1:mx).
+
+    implicit none
+    
+    integer, intent(in) :: meqn,mbc,mx,maux
+    real(kind=8), intent(in) :: xlower,dx
+    real(kind=8), intent(in) :: aux(maux,1-mbc:mx+mbc)
+    real(kind=8), intent(inout) :: q(meqn,1-mbc:mx+mbc)
+
+    write(6,*) '*** Error -- you must set initial conditions'
+    stop
+
+end subroutine qinit
