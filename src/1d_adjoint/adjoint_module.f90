91d872b25841457e24fa64e2ed148b064eff9a36
Brisa Davis <bndavis@uw.edu>
2017-06-29 09:32:46 -0700

    Refactoring code to use adjoint_output rather than adjointFolder, and write more things to adjoint.data.

--- src/1d_adjoint/adjoint_module.f90
+++ src/1d_adjoint/adjoint_module.f90
@@ -41,27 +41,36 @@ module adjoint_module
 
 contains
 
-    subroutine read_adjoint_data(adjointFolder)
+    subroutine read_adjoint_data()
 
         use amr_reload_module
         implicit none
 
         ! Function Arguments
         character(len=*), parameter :: adjointfile = 'adjoint.data'
-        character(len=*), intent(in) :: adjointFolder
+        character(len=200) :: adjoint_output
         logical :: fileExists
         integer :: iunit, k
         real(kind=8) :: finalT
+        real(kind=8) :: t1,t2
 
         adjoint_flagging = .true.
 
         inquire(file=adjointfile, exist=fileExists)
         if (fileExists) then
 
-            ! Reload adjoint files
-            call amr1_reload(adjointFolder)
             iunit = 16
             call opendatafile(iunit,adjointfile)
+            read(iunit,*) adjoint_output
+
+            ! time period of interest:
+            read(iunit,*) t1
+            read(iunit,*) t2
+
+            call set_time_window(t1, t2)
+
+            ! Reload adjoint files
+            call amr1_reload(trim(adjoint_output))
 
             read(iunit,*) totnum_adjoints
             read(iunit,*) innerprod_index
