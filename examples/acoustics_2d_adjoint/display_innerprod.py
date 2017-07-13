a2a2e3bbb08713b30b5c3adab47cb1a2f0fe5174
Brisa Davis <bndavis@uw.edu>
2017-07-13 12:36:36 -0700

    Fixing indents in file.

--- examples/acoustics_2d_adjoint/display_innerprod.py
+++ examples/acoustics_2d_adjoint/display_innerprod.py
@@ -15,7 +15,7 @@ def main():
     forward_frameno = 8
     adjoint_frameno = 7
     
-    # Number of levels in the simulation
+    # Number of levels to consider
     levels = [1,2,3]
     # ----------------------------------
 
@@ -93,67 +93,67 @@ def calculate_innerprod(q,innerprod,x,y,i,j,framesoln_a):
         xc_centers_a, yc_centers_a = patch_a.grid.c_centers
 
 
-    if ((x > xlower_a) and (x < xupper_a)
-        and (y > ylower_a) and (y < yupper_a)):
-        # finding correct cells to interpolate with
-        idx = (np.abs(xc_centers_a[:,0]-x)).argmin()
-        idy = (np.abs(yc_centers_a[0,:]-y)).argmin()
+        if ((x > xlower_a) and (x < xupper_a)
+            and (y > ylower_a) and (y < yupper_a)):
+            # finding correct cells to interpolate with
+            idx = (np.abs(xc_centers_a[:,0]-x)).argmin()
+            idy = (np.abs(yc_centers_a[0,:]-y)).argmin()
             
-        if (xc_centers_a[idx,0] <= x):
-            idxm = idx
-            idxp = idx + 1
-            if (idxp >= mx_a):
-                idxp = idx
-        else:
-            idxm = idx - 1
-            idxp = idx
-            if (idxm < 0):
+            if (xc_centers_a[idx,0] <= x):
                 idxm = idx
+                idxp = idx + 1
+                if (idxp >= mx_a):
+                    idxp = idx
+            else:
+                idxm = idx - 1
+                idxp = idx
+                if (idxm < 0):
+                    idxm = idx
                 
-        if (yc_centers_a[0,idy] <= y):
-            idym = idy
-            idyp = idy + 1
-            if (idyp >= my_a):
-                idyp = idy
-        else:
-            idym = idy - 1
-            idyp = idy
-            if (idym < 0):
+            if (yc_centers_a[0,idy] <= y):
                 idym = idy
-                
-        # interpolate in x
-        xinterp = False
-        if (idxm != idxp):
-            xinterp = True
-                        
-            xp = xc_centers_a[idxp,0]
-            xm = xc_centers_a[idxm,0]
-            denomx = xp - xm
-                        
-            q_interp1 = ((xp - x)/denomx)*q_a[:,idxp,idyp] + ((x - xm)/denomx)*q_a[:,idxm,idyp]
-            q_interp2 = ((xp - x)/denomx)*q_a[:,idxp,idym] + ((x - xm)/denomx)*q_a[:,idxm,idym]
-                
-        # interpolate in y
-        if (idym != idyp):
-            yp = yc_centers_a[0,idyp]
-            ym = yc_centers_a[0,idym]
-            denomy = yp - ym
-                        
-            if (xinterp):
-                q_interp = ((yp - y)/denomy)*q_interp1 + ((y - ym)/denomy)*q_interp2
+                idyp = idy + 1
+                if (idyp >= my_a):
+                    idyp = idy
             else:
-                q_interp = ((yp - y)/denomy)*q_a[:,idxp,idyp] + ((y - ym)/denomy)*q_a[:,idxp,idym]
-        else:
-            if (xinterp):
-                q_interp = q_interp1
+                idym = idy - 1
+                idyp = idy
+                if (idym < 0):
+                    idym = idy
+                
+            # interpolate in x
+            xinterp = False
+            if (idxm != idxp):
+                xinterp = True
+                        
+                xp = xc_centers_a[idxp,0]
+                xm = xc_centers_a[idxm,0]
+                denomx = xp - xm
+                        
+                q_interp1 = ((xp - x)/denomx)*q_a[:,idxp,idyp] + ((x - xm)/denomx)*q_a[:,idxm,idyp]
+                q_interp2 = ((xp - x)/denomx)*q_a[:,idxp,idym] + ((x - xm)/denomx)*q_a[:,idxm,idym]
+                
+            # interpolate in y
+            if (idym != idyp):
+                yp = yc_centers_a[0,idyp]
+                ym = yc_centers_a[0,idym]
+                denomy = yp - ym
+                        
+                if (xinterp):
+                    q_interp = ((yp - y)/denomy)*q_interp1 + ((y - ym)/denomy)*q_interp2
+                else:
+                    q_interp = ((yp - y)/denomy)*q_a[:,idxp,idyp] + ((y - ym)/denomy)*q_a[:,idxp,idym]
             else:
-                q_interp = q_a[:,idxp,idyp]
+                if (xinterp):
+                    q_interp = q_interp1
+                else:
+                    q_interp = q_a[:,idxp,idyp]
                             
-        # calculating inner product
+            # calculating inner product
                             
-        # leaving off the last term in q, because that is the inner product
-        # that was calculated during the actual Clawpack run
-        innerprod[i,j] = abs(np.sum(q[:-1,i,j]*q_interp))
+            # leaving off the last term in q, because that is the inner product
+            # that was calculated during the actual Clawpack run
+            innerprod[i,j] = abs(np.sum(q[:-1,i,j]*q_interp))
 
 if __name__ == '__main__':
     main()
