6a335b536ddb63bbe92fdbea850a26dc03b52c58
Brisa Davis <bndavis@uw.edu>
2017-08-18 14:51:39 -0700

    Updating 1d code to read in adjoint solution from binary files.

--- src/1d_adjoint/interp_adjoint.f90
+++ src/1d_adjoint/interp_adjoint.f90
@@ -2,14 +2,15 @@
 ! :::::     Routine to interpolate adjoint to given x,y point
 ! ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-subroutine interp_adjoint(lst, lend, nvar, x, q, k)
+subroutine interp_adjoint(nvar, x, q, k)
 
         use adjoint_module, only: adjoints
-        use amr_reload_module
+
+        implicit none
 
         ! Function arguments
-        integer, intent(in) :: lst, lend, k
-        integer :: ii_c, ii_a, nvar
+        integer, intent(in) :: k
+        integer :: ii_c, ii_a, nvar, z
         real(kind=8), intent(in) :: x
         integer :: nx,loc,level,mptr,mitot, &
                 ivar,i, iadd, iaddaux
@@ -18,72 +19,68 @@ subroutine interp_adjoint(lst, lend, nvar, x, q, k)
         real(kind=8) :: q(nvar+1),q_temp1(nvar+1), &
                 q_temp2(nvar+1), denom
 
-        iadd(ivar,i)  = loc + ivar - 1 + nvar*(i-1)
+        iadd(ivar,i)  = loc + ivar - 1 + adjoints(k)%meqn*(i-1)
 
-        do ivar=1,nvar+1
+        do ivar=1,nvar
             q(ivar) = 0.0
         enddo
 
-        level = lst
-65      if (level .gt. lend) go to 90
-            mptr = adjoints(k)%lstart(level)
-70          if (mptr .eq. 0) go to 80
-                ! Number of points in x (nx)
-                nx = adjoints(k)%node(ndihi,mptr) - adjoints(k)%node(ndilo,mptr) + 1
+        do z = 1, adjoints(k)%ngrids
+            mptr = adjoints(k)%gridpointer(z)
+            level = adjoints(k)%gridlevel(mptr)
 
-                ! Finding x extreem values for grid
-                xlow = adjoints(k)%rnode(cornxlo,mptr)
-                dx = adjoints(k)%hxposs(level)
-                xhi = xlow + nx*dx
+            ! Number of points in x (nx)
+            nx = adjoints(k)%ncellsx(mptr)
 
-                loc     = adjoints(k)%node(store1, mptr)
+            ! Finding x extreem values for grid
+            xlow = adjoints(k)%xlowvals(mptr)
+            dx = adjoints(k)%hxposs(level)
+            xhi = xlow + nx*dx
 
-                ! Total number of points in x
-                mitot = nx + 2*nghost
+            loc     = adjoints(k)%loc(mptr)
 
-                if ((x < xlow) .or. (x > xhi)) then
-                    ! Skipping interpolation if the point of interest
-                    ! is not in the current grid
-                    continue
-                else
-                    xm = xlow - (nghost+0.5d0)*dx
+            ! Total number of points in x
+            mitot = nx + 2*adjoints(k)%nghost
 
-                    ! Finding current cell in x (i)
-                    ii_c = int((x-xm)/dx)
+            if ((x < xlow) .or. (x > xhi)) then
+                ! Skipping interpolation if the point of interest
+                ! is not in the current grid
+                continue
+            else
+                xm = xlow - (adjoints(k)%nghost+0.5d0)*dx
 
-                    ! Finding correct cell to interpolate with
-                    ii_a = int(((x-xm)/dx) + 0.5d0)
+                ! Finding current cell in x (i)
+                ii_c = int((x-xm)/dx)
 
-                    if (ii_c == ii_a .and. ii_a /= 0) then
-                        ii_a = ii_a - 1
-                    endif
-                    if (ii_a >= nx) then
-                        ii_a = ii_a - 1
-                    endif
+                ! Finding correct cell to interpolate with
+                ii_a = int(((x-xm)/dx) + 0.5d0)
 
-
-                    ! Interpolating in x
-                    x_main = xm + (ii_c + 0.5d0)*dx
-                    if (ii_c /= ii_a) then
-                        x_side = xm + (ii_a + 0.5d0)*dx
-                        denom = x_side - x_main
-
-                        do ivar=1,nvar
-                            q(ivar) = &
-                                ((x_side - x)/denom)*adjoints(k)%alloc(iadd(ivar,ii_c)) &
-                                + ((x - x_main)/denom)*adjoints(k)%alloc(iadd(ivar,ii_a))
-                        enddo
-                    else
-                        do ivar=1,nvar
-                            q(ivar) = adjoints(k)%alloc(iadd(ivar,ii_c))
-                        enddo
-                    endif
+                if (ii_c == ii_a .and. ii_a /= 0) then
+                    ii_a = ii_a - 1
+                endif
+                if (ii_a >= nx) then
+                    ii_a = ii_a - 1
                 endif
 
-            mptr = adjoints(k)%node(levelptr, mptr)
-            go to 70
-80      level = level + 1
-        go to 65
-90   continue
+
+                ! Interpolating in x
+                x_main = xm + (ii_c + 0.5d0)*dx
+                if (ii_c /= ii_a) then
+                    x_side = xm + (ii_a + 0.5d0)*dx
+                    denom = x_side - x_main
+
+                    do ivar=1,nvar
+                        q(ivar) = &
+                            ((x_side - x)/denom)*adjoints(k)%alloc(iadd(ivar,ii_c)) &
+                            + ((x - x_main)/denom)*adjoints(k)%alloc(iadd(ivar,ii_a))
+                    enddo
+                else
+                    do ivar=1,nvar
+                        q(ivar) = adjoints(k)%alloc(iadd(ivar,ii_c))
+                    enddo
+                endif
+            endif
+
+        enddo
 
       end subroutine interp_adjoint
