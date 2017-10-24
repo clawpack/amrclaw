7977dcb29ced59ecbb46c06bad433458bbb39d39
Xinsheng (Shawn) Qin <xinshengqin2014@gmail.com>
2017-10-23 21:16:10 -0700

    add ability to allocate 4-rank array to memory_module

--- src/2d/memory_module.f90
+++ src/2d/memory_module.f90
@@ -10,11 +10,13 @@ module memory_module
         module procedure gpu_allocate_r1
         module procedure gpu_allocate_r2
         module procedure gpu_allocate_r3
+        module procedure gpu_allocate_r4
     end interface
     interface gpu_deallocate
         module procedure gpu_deallocate_r1
         module procedure gpu_deallocate_r2
         module procedure gpu_deallocate_r3
+        module procedure gpu_deallocate_r4
     end interface
 
 
@@ -97,6 +99,34 @@ contains
         end subroutine shift_bound_d3
     end subroutine gpu_allocate_r3
 
+    subroutine gpu_allocate_r4(a, dev_id, lo1, hi1, lo2, hi2, lo3, hi3, lo4, hi4)
+        use cudafor
+        real(kind=8), pointer, device, intent(inout) :: a(:,:,:,:)
+        integer, intent(in) :: lo1, hi1, lo2, hi2, lo3, hi3, lo4, hi4
+        integer, intent(in) :: dev_id
+        integer :: n1, n2, n3, n4
+        integer (kind=c_size_t) :: sz
+        type(c_devptr) :: cp
+        real(kind=8), pointer, device :: fp(:,:,:,:)
+        integer :: istat
+        n1 = max(hi1-lo1+1, 1)
+        n2 = max(hi2-lo2+1, 1)
+        n3 = max(hi3-lo3+1, 1)
+        n4 = max(hi4-lo4+1, 1)
+        sz = int(n1,c_size_t) * int(n2,c_size_t) * int(n3,c_size_t) * int(n4,c_size_t)
+        istat = cudaSetDevice(dev_id)
+        istat = cudaMalloc(cp, szr*sz)
+        call c_f_pointer(cp, fp, shape=(/n1,n2,n3,n4/))
+        call shift_bound_d4(fp, lo1, lo2, lo3, lo4, a)
+    contains
+        subroutine shift_bound_d4(fp, lo1, lo2, lo3, lo4, a)
+            integer, intent(in) :: lo1, lo2, lo3, lo4
+            real(kind=8), target,  device, intent(in) :: fp(lo1:,lo2:,lo3:,lo4:)
+            real(kind=8), pointer, device, intent(inout) :: a(:,:,:,:)
+            a => fp
+        end subroutine shift_bound_d4
+    end subroutine gpu_allocate_r4
+
     subroutine gpu_deallocate_r1(a)
         use cudafor
         real(kind=8), pointer, device, intent(inout) :: a(:)
@@ -130,6 +160,16 @@ contains
         endif
     end subroutine gpu_deallocate_r3
 
+    subroutine gpu_deallocate_r4(a)
+        use cudafor
+        real(kind=8), pointer, device, intent(inout) :: a(:,:,:,:)
+        integer :: istat
+        istat = cudaFree(a)
+        if (istat .ne. cudaSuccess) then
+            write(*,*) 'CUDA  error:', cudaGetErrorString(cudaGetLastError())
+            stop
+        endif
+    end subroutine gpu_deallocate_r4
 
 
 end module memory_module
