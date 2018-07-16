54d51ae8bd85addac4a3a43d74b1c5986ab377d4
Xinsheng (Shawn) Qin <xinshengqin2014@gmail.com>
2018-07-15 23:50:38 -0700

    change how max1d affects bisect grid

--- src/2d/birect.f90
+++ src/2d/birect.f90
@@ -1,62 +1,67 @@
-c
+!
 !> Check each grid, starting with **mptr1** (either newstl or lstart)
 !! to see that it has no more than max1d points in either dimensions.
 !! needed so that scratch array space in stepgrid not exceeded.
 !!
 !! Also check for too small grids - but has never happened.
-c
-c --------------------------------------------------
-c
-      subroutine birect(mptr1)
-c
-      use amr_module
-      implicit real(CLAW_REAL) (a-h,o-z)
+!
+! --------------------------------------------------
+!
+subroutine birect(mptr1)
+    !
+    use amr_module
+    implicit real(CLAW_REAL) (a-h,o-z)
+    integer :: total_cells
 
 
-c
-c :::::::::::::  BIRECT :::::::::::::::::::::::::::::::::::::::
-c check each grid, starting with mptr1 (either newstl or lstart)
-c to see that it has no more than max1d points in either dimensions.
-c needed so that scratch array space in stepgrid not exceeded.
-c
-c also check for too small grids - but has never happened.
-c :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-c
-      mptr  = mptr1
-      level = node(nestlevel,mptr)
-      hx    = hxposs(level)
-      hy    = hyposs(level)
-c
-10    continue
-      cxlo    = rnode(cornxlo,mptr)
-      cxhi    = rnode(cornxhi,mptr)
-      cylo    = rnode(cornylo,mptr)
-      cyhi    = rnode(cornyhi,mptr)
-      nx      = node(ndihi,mptr) - node(ndilo,mptr) + 1
-      ny      = node(ndjhi,mptr) - node(ndjlo,mptr) + 1
-      minsize = 2*nghost
-c
-c check number of rows first - if too many, bisect grid with vertical
-c line down the middle. make sure new grid corners are anchored
-c on coarse grid point. make sure if bisecting coarse grid that
-c new grids have even number of points
-c
-      if (nx + 2*nghost .gt. max1d) then
- 
+    !
+    ! :::::::::::::  BIRECT :::::::::::::::::::::::::::::::::::::::
+    ! check each grid, starting with mptr1 (either newstl or lstart)
+    ! to see that it has no more than max1d points in either dimensions.
+    ! needed so that scratch array space in stepgrid not exceeded.
+    !
+    ! also check for too small grids - but has never happened.
+    ! :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    !
+    mptr  = mptr1
+    level = node(nestlevel,mptr)
+    hx    = hxposs(level)
+    hy    = hyposs(level)
+    !
+    10    continue
+    cxlo    = rnode(cornxlo,mptr)
+    cxhi    = rnode(cornxhi,mptr)
+    cylo    = rnode(cornylo,mptr)
+    cyhi    = rnode(cornyhi,mptr)
+    nx      = node(ndihi,mptr) - node(ndilo,mptr) + 1
+    ny      = node(ndjhi,mptr) - node(ndjlo,mptr) + 1
+    minsize = 2*nghost
+    !
+    ! check number of rows first - if too many, bisect grid with vertical
+    ! line down the middle. make sure new grid corners are anchored
+    ! on coarse grid point. make sure if bisecting coarse grid that
+    ! new grids have even number of points
+    !
+    total_cells = (nx+2*nghost)*(ny+2*nghost)
+    if ( &
+         (nx + 2*nghost .gt. max1d ) &
+         .and. (total_cells > max1d*max1d/2) &
+       ) then
+
         nxl    = nx/2
         if (level .gt. 1) then 
-           lratio = intratx(level-1)
+            lratio = intratx(level-1)
         else
-           lratio = 2
+            lratio = 2
         endif
         nxl = (nxl/lratio)*lratio 
         nxr    = nx - nxl 
         cxmid  = cxlo + nxl*hx
- 
+
         mptrnx = nodget()
         node(levelptr,mptrnx) = node(levelptr,mptr)
         node(levelptr,mptr)   = mptrnx
- 
+
         rnode(cornxhi,mptr) = cxmid
         node(ndihi,mptrnx)  = node(ndihi,mptr)
         node(ndihi,mptr)    = node(ndilo,mptr) + nxl - 1
@@ -72,26 +77,29 @@ c
         node(nestlevel,mptrnx)   = node(nestlevel,mptr)
 
         go to 10
-c
-c check number of columns next - if too many, bisect grid with horizontal
-c line down the middle
-c
-      else if (ny + 2*nghost .gt. max1d) then
- 
+        !
+        ! check number of columns next - if too many, bisect grid with horizontal
+        ! line down the middle
+        !
+    else if ( &
+              (ny + 2*nghost .gt. max1d) &
+              .and. (total_cells > max1d*max1d/2) &
+            ) then
+
         nyl    = ny/2
         if (level .gt. 1) then 
-           lratio = intraty(level-1)
+            lratio = intraty(level-1)
         else
-           lratio = 2
+            lratio = 2
         endif
         nyl = (nyl/lratio)*lratio
         nyr    = ny - nyl 
         cymid  =  cylo + nyl*hy
- 
+
         mptrnx = nodget()
         node(levelptr,mptrnx) = node(levelptr,mptr)
         node(levelptr,mptr)   = mptrnx
- 
+
         rnode(cornyhi,mptr)   = cymid
 
         node(ndjhi,mptrnx) = node(ndjhi,mptr)
@@ -107,14 +115,14 @@ c
         node(nestlevel,mptrnx)  = node(nestlevel,mptr)
         rnode(timemult,mptrnx)  = rnode(timemult,mptr)
         go to 10
-c
-c  grid ok - check the next
-c
-      else
+        !
+        !  grid ok - check the next
+        !
+    else
         mptr = node(levelptr,mptr)
         if (mptr.ne.0) go to 10
- 
-      endif
- 
-      return
-      end
+
+    endif
+
+    return
+end
--- src/2d/saveqc.f
+++ src/2d/saveqc.f
@@ -23,7 +23,7 @@ c
       
       logical sticksout, found
 !     make fliparray largest possible grid size
-      dimension fliparray(2*max1d*nghost*(nvar+naux))
+      dimension fliparray(2*max1d*max1d/2*nghost*(nvar+naux))
 c
 c ::::::::::::::::::::::::: SAVEQC :::::::::::::::::::::::::::::::::
 c  prepare new fine grids to save fluxes after each integration step
