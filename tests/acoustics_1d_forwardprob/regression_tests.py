f87365e44c770bf798160bb54c3a2460a150fe7a
Randy LeVeque <rjl@uw.edu>
2018-08-07 15:35:51 -0700

    fix path in tests/acoustics_1d_forwardprob

--- tests/acoustics_1d_forwardprob/regression_tests.py
+++ tests/acoustics_1d_forwardprob/regression_tests.py
@@ -1,29 +1,32 @@
 """
-Regression tests for 2D advection on a square.
+Regression tests for 1D acoustics with adjoint.
 """
 
+from __future__ import print_function
 from __future__ import absolute_import
 import sys,os
 import unittest
 
+thisfile = os.path.realpath(__file__)
+testdir = os.path.split(thisfile)[0]
+
 import clawpack.amrclaw.test as test
 
 
-class Acoustics2DAdjointTest(test.AMRClawRegressionTest):
-    r"""Basic test for a 2D acoustics adjoint-flagging forward problem test case"""
+class Acoustics1DAdjointTest(test.AMRClawRegressionTest):
+    r"""Basic test for a 1D acoustics adjoint-flagging forward problem test case"""
 
 
     def runTest(self, save=False):
         
         # Run adjoint problem
-        currentdir = os.getcwd()
-        adjointdir = currentdir + '/adjoint'
+        adjointdir = testdir + '/adjoint'
 
         # Running the adjoint problem
         os.chdir(adjointdir)
         os.system('make -s new')
         os.system('make .output > /dev/null')
-        os.chdir(currentdir)
+        os.chdir(testdir)
 
         # Write out data files
         self.load_rundata()
@@ -50,7 +53,7 @@ if __name__=="__main__":
     if len(sys.argv) > 1:
         if bool(sys.argv[1]):
             # Fake the setup and save out output
-            test = Acoustics2DAdjointTest()
+            test = Acoustics1DAdjointTest()
             try:
                 test.setUp()
                 test.runTest(save=True)
