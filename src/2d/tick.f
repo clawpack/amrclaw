78aa9c91eec54c6085c65fa994fed7a22297f96c
Xinsheng (Shawn) Qin <xinshengqin2014@gmail.com>
2018-10-07 20:43:29 -0700

    add timer for advance for all levels

--- src/2d/tick.f
+++ src/2d/tick.f
@@ -257,6 +257,10 @@ c
 
 
 #ifdef PROFILE
+          call take_cpu_timer("Advance (all levels)", 
+     &      timer_advanc_all_levels)
+          call cpu_timer_start(timer_advanc_all_levels)
+
           call take_cpu_timer("Advance level "//toString(level), 
      &      timer_advanc_start+level-1)
           call cpu_timer_start(timer_advanc_start+level-1)
@@ -264,6 +268,7 @@ c
           call advanc(level,nvar,dtlevnew,vtime,naux)
 #ifdef PROFILE
           call cpu_timer_stop(timer_advanc_start+level-1)
+          call cpu_timer_stop(timer_advanc_all_levels)
 #endif
 
 c         # rjl modified 6/17/05 to print out *after* advanc and print cfl
--- src/2d/timer_module.f90
+++ src/2d/timer_module.f90
@@ -61,10 +61,11 @@ module timer_module
     integer, parameter :: timer_saveqc = 13
     integer, parameter :: timer_conck = 14
     integer, parameter :: timer_memory = 15
+    integer, parameter :: timer_advanc_all_levels = 16 
     ! time for calling advanc function
     ! starting from timer_advanc_start, the ids are reserved
     ! for timing level 1,2,3,4 ...
-    integer, parameter :: timer_advanc_start = 16 
+    integer, parameter :: timer_advanc_start = 17 
 
 contains
     subroutine take_cpu_timer(timer_name_, timer_id)
