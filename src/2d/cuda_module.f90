e51accd80a596458e26cce319fa6d41c0b761c8f
Xinsheng (Shawn) Qin <xinshengqin2014@gmail.com>
2018-10-27 15:27:59 -0700

    output more GPU info

--- src/2d/cuda_module.f90
+++ src/2d/cuda_module.f90
@@ -84,15 +84,7 @@ contains
 
         do p = 0, num_devices-1
             cudaResult = cudaGetDeviceProperties(prop, p)
-            write(*,"(' Device Number: ',i0)") p
-            write(*,"('   Device name: ',a)") trim(prop%name)
-            write(*,"('   Memory Clock Rate (KHz): ', i0)") &
-              prop%memoryClockRate
-            write(*,"('   Memory Bus Width (bits): ', i0)") &
-              prop%memoryBusWidth
-            write(*,"('   Peak Memory Bandwidth (GB/s): ', f6.2)") &
-              2.0*prop%memoryClockRate*(prop%memoryBusWidth/8)/10.0**6
-            write(*,*)        
+            call printDeviceProperties(prop,p)
         enddo
 
         call opendatafile(inunit,gpufile)
@@ -721,5 +713,49 @@ contains
             enddo
         enddo
     end subroutine soa_to_aos_r2
+
+    subroutine printDeviceProperties(prop, num)
+	use cudafor
+        implicit none
+	type(cudadeviceprop) :: prop
+	integer :: num,ilen
+	ilen = verify(prop%name, ' ', .true.)
+	write (*,900) "Device Number: " ,num
+	write (*,901) "Device Name: " ,prop%name(1:ilen)
+	write (*,903) "Total Global Memory: ",real(prop%totalGlobalMem)/1e9," Gbytes"
+        write(*,"('Memory Clock Rate (KHz): ', i0)") &
+              prop%memoryClockRate
+        write(*,"('Memory Bus Width (bits): ', i0)") &
+              prop%memoryBusWidth
+        write(*,"('Peak Memory Bandwidth (GB/s): ', f6.2)") &
+              2.0*prop%memoryClockRate*(prop%memoryBusWidth/8)/10.0**6
+	write (*,902) "sharedMemPerBlock: " ,prop%sharedMemPerBlock," bytes"
+	write (*,900) "regsPerBlock: " ,prop%regsPerBlock
+	write (*,900) "warpSize: " ,prop%warpSize
+	write (*,900) "maxThreadsPerBlock: " ,prop%maxThreadsPerBlock
+	write (*,904) "maxThreadsDim: " ,prop%maxThreadsDim
+	write (*,904) "maxGridSize: " ,prop%maxGridSize
+	write (*,903) "ClockRate: " ,real(prop%clockRate)/1e6," GHz"
+	write (*,902) "Total Const Memory: " ,prop%totalConstMem," bytes"
+	write (*,905) "Compute Capability Revision: ",prop%major,prop%minor
+	write (*,902) "TextureAlignment: " ,prop%textureAlignment," bytes"
+	write (*,906) "deviceOverlap: " ,prop%deviceOverlap
+	write (*,900) "multiProcessorCount: ",prop%multiProcessorCount
+	write (*,906) "integrated: " ,prop%integrated
+	write (*,906) "canMapHostMemory: " ,prop%canMapHostMemory
+	write (*,906) "ECCEnabled: " ,prop%ECCEnabled
+	write (*,906) "UnifiedAddressing: " ,prop%unifiedAddressing
+	write (*,900) "L2 Cache Size: " ,prop%l2CacheSize
+	write (*,900) "maxThreadsPerSMP: " ,prop%maxThreadsPerMultiProcessor
+	900 format (a,i0)
+	901 format (a,a)
+	902 format (a,i0,a)
+	903 format (a,f8.3,a)
+	904 format (a,2(i0,1x,'x',1x),i0)
+	905 format (a,i0,'.',i0)
+	906 format (a,l0)
+        write(*,*)        
+	return
+    end
 
 end module cuda_module
