58a2c734d9756d32b19780431fe6a8694a275d97
Xinsheng (Shawn) Qin <xinshengqin2014@gmail.com>
2018-11-04 19:19:21 -0800

    set parameters like dry_tol from Fortran

--- src/2d/cudaclaw/advance.cu
+++ src/2d/cudaclaw/advance.cu
@@ -1,1 +1,20 @@
 #include "advance.H"
+#include "real.H"
+#include "common.H"
+
+// some global constants that need to be set from Fortran
+
+
+#ifdef GEOCLAW
+// the dry_tolerance parameter in setrun.py 
+__device__ __constant__  real dry_tolerance = claw_nan; // default value. Can be set from Fortran
+
+void set_dry_tolerance (real value)
+{
+    cudaMemcpyToSymbol(dry_tolerance,&value,sizeof(real));
+}
+#endif
+
+
+
+
