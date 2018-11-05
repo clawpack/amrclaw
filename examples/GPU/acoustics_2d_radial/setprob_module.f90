33d389f3dcd42726d819c67dd90ba5263f554ca7
Xinsheng (Shawn) Qin <xinshengqin2014@gmail.com>
2018-11-04 20:43:12 -0800

    setup setprob_module.f90 for acoustics example

--- examples/GPU/acoustics_2d_radial/setprob_module.f90
+++ examples/GPU/acoustics_2d_radial/setprob_module.f90
@@ -1,33 +1,35 @@
-module problem_para_module
+module setprob_module
 
-    real(CLAW_REAL), save :: rho, bulk, cc, zz
+    save
+
+    real(CLAW_REAL) :: rho, bulk, cc, zz
+#ifdef CUDA
+    real(CLAW_REAL), constant :: rho_d, bulk_d ! in device constant memory
+#endif
 
     contains
     subroutine setprob
         implicit real(CLAW_REAL) (a-h,o-z)
         character*25 fname
-        ! common /cparam/ rho,bulk,cc,zz
-
         !
         !     # Set the material parameters for the acoustic equations
-        !
         !
         iunit = 7
         fname = 'setprob.data'
         !     # open the unit with new routine from Clawpack 4.4 to skip over
         !     # comment lines starting with #:
         call opendatafile(iunit, fname)
-
-        !
         !     # Density and bulk modulus:
-
         read(7,*) rho
         read(7,*) bulk
-        !
         !     # Compute sound speed and impendance:
-
         cc = sqrt(bulk/rho)
         zz = rho*cc
+
+#ifdef CUDA
+        rho_d = rho
+        bulk_d = bulk
+#endif
 
         return
     end subroutine
