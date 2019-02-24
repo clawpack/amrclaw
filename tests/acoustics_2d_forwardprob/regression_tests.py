9abae05c391077e8fb262c9603c876db8e3ba199
Randy LeVeque <rjl@uw.edu>
2019-02-24 14:56:25 -0800

    update ests/acoustics_2d_forwardprob with new setrun, different parameters,
    Also added adjoint/regression_data directory that is not automatically
    used in nose tests but available if needed for debugging whether adjoint
    solution changed in case regression tests for forward problem don't pass.

--- tests/acoustics_2d_forwardprob/regression_tests.py
+++ tests/acoustics_2d_forwardprob/regression_tests.py
@@ -36,14 +36,8 @@ class Acoustics2DAdjointTest(test.AMRClawRegressionTest):
         self.run_code()
 
         # Perform tests
-        self.check_gauges(save=save, gauge_id=0)
         self.check_gauges(save=save, gauge_id=1)
         self.check_gauges(save=save, gauge_id=2)
-
-        # self.check_gauges(save=save, gauge_num=1,
-        #                   regression_data_path='regression_data_test2.txt')
-        # self.check_gauges(save=save, gauge_num=2,
-        #                   regression_data_path='regression_data_test3.txt')
 
         self.success = True
 
