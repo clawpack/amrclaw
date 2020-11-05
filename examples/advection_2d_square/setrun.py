5e1474bd9e8431563f5ce68bc955806e95df892c
Randy LeVeque <rjl@uw.edu>
2020-11-05 13:44:26 -0800

    advection_2d_square/setrun.py with suitable memsize as example

--- examples/advection_2d_square/setrun.py
+++ examples/advection_2d_square/setrun.py
@@ -113,8 +113,8 @@ def setrun(claw_pkg='amrclaw'):
     if clawdata.output_style==1:
         # Output ntimes frames at equally spaced times up to tfinal:
         # Can specify num_output_times = 0 for no output
-        clawdata.num_output_times = 1
-        clawdata.tfinal = 0.2
+        clawdata.num_output_times = 10
+        clawdata.tfinal = 2.0
         clawdata.output_t0 = True  # output at initial (or restart) time?
         
     elif clawdata.output_style == 2:
@@ -270,7 +270,7 @@ def setrun(claw_pkg='amrclaw'):
     amrdata = rundata.amrdata
 
     # memsize is initial length of alloc array used for AMR patches
-    amrdata.memsize = 69200
+    amrdata.memsize = 100000
 
     # max1d controls size of grids
     amrdata.max1d = 60
