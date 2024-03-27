626922645afe199878708e31726d3714b764c128
Randy LeVeque <rjl@uw.edu>
2024-03-27 13:00:49 -0700

    clean up run_tests.py

--- examples/run_tests.py
+++ examples/run_tests.py
@@ -10,14 +10,23 @@ import os
 env = os.environ
 env['GIT_STATUS'] = 'True'
 env['FFLAGS'] = '-O2 -fopenmp'
-env['OMP_NUM_THREADS'] = '3'
+env['OMP_NUM_THREADS'] = '6'
 
 make_all.make_all(make_clean_first=True, env=env)
 
+run_notebooks = True  # run any *.ipynb files and create html versions?
+
+if run_notebooks:
+    make_all.make_notebook_htmls(env=env)
+
+
 print("\n-----------------------------------------------------------\n")
 
-all_ok = regression_tests.test_subdirs()
-if all_ok:
-    print("===> All tests pass")
-else:
-    print("===> Some test(s) failed")
+if 0:
+    # Not currently working since gallery links changed
+    # Needs more general rethinking
+    all_ok = regression_tests.test_subdirs()
+    if all_ok:
+        print("===> All tests pass")
+    else:
+        print("===> Some test(s) failed")
