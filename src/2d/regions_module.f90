6d597e18f7e9b2514b11a008c65618999bf2cc4f
Katherine (Katy) Barnhart <krbarnhart@usgs.gov>
2025-02-11 11:18:22 -0700

    switch i2 to i3

--- src/2d/regions_module.f90
+++ src/2d/regions_module.f90
@@ -63,7 +63,7 @@ contains
                 call opendatafile(unit,'regions.data')
             endif
 
-            read(unit,"(i2)") num_regions
+            read(unit,"(i3)") num_regions
             if (num_regions == 0) then
                 write(parmunit,*) '  No regions specified for refinement'
                 
@@ -118,7 +118,7 @@ contains
             call opendatafile(unit,'flagregions.data')
         endif
 
-        read(unit,"(i2)") num_rregions
+        read(unit,"(i3)") num_rregions
         if (num_rregions == 0) then
             write(parmunit,*) '  No ruled regions specified for refinement'
             
