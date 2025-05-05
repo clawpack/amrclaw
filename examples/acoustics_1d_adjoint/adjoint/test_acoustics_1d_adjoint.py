49622b3c5a7597a16ebfb0d29a6afe0f19a6a0ac
Kyle Mandli <kyle.mandli@gmail.com>
2025-05-05 10:21:12 -0400

    Rework 1d adjoint test

--- examples/acoustics_1d_adjoint/adjoint/test_acoustics_1d_adjoint.py
+++ examples/acoustics_1d_adjoint/adjoint/test_acoustics_1d_adjoint.py
@@ -8,15 +8,15 @@ import unittest
 import clawpack.amrclaw.test as test
 import clawpack.amrclaw.data as data
 
-class Acoustics1DAdjointForwardTest(test.AMRClawRegressionTest):
+class Acoustics1DAdjointTest(test.AMRClawRegressionTest):
 
     def runTest(self, save=False):
 
         # Write out data files
         self.load_rundata()
         
-        self.rundata.clawdata.num_output_times = 10
-        self.rundata.clawdata.tfinal = 1.0
+        # self.rundata.clawdata.num_output_times = 10
+        # self.rundata.clawdata.tfinal = 1.0
 
         # self.rundata.clawdata.use_fwaves = False
 
@@ -35,7 +35,7 @@ if __name__=="__main__":
     if len(sys.argv) > 1:
         if bool(sys.argv[1]):
             # Fake the setup and save out output
-            test = Acoustics1DAdjointForwardTest()
+            test = Acoustics1DAdjointTest()
             try:
                 test.setUp()
                 test.runTest(save=True)
--- examples/acoustics_1d_adjoint/test_acoustics_1d_adjoint_forward.py
+++ examples/acoustics_1d_adjoint/test_acoustics_1d_adjoint_forward.py
@@ -10,10 +10,10 @@ import unittest
 import clawpack.amrclaw.test as test
 import clawpack.clawutil.runclaw
 
-from adjoint.test_acoustics_1d_adjoint_forward import Acoustics1DAdjointForwardTest 
+from adjoint.test_acoustics_1d_adjoint import Acoustics1DAdjointTest
 
 
-class Acoustics1DAdjointTest(test.AMRClawRegressionTest):
+class Acoustics1DAdjointForwardTest(test.AMRClawRegressionTest):
     r"""Basic test for a 1D acoustics adjoint-flagging forward problem test case"""
 
 
@@ -21,7 +21,7 @@ class Acoustics1DAdjointTest(test.AMRClawRegressionTest):
         
         # Run adjoint problem
         try:
-            adjoint_run = Acoustics1DAdjointForwardTest()    
+            adjoint_run = Acoustics1DAdjointTest()    
             adjoint_run.setUp()
             adjoint_run.runTest()
             
@@ -37,15 +37,15 @@ class Acoustics1DAdjointTest(test.AMRClawRegressionTest):
         # Write out data files
         self.load_rundata()
 
-        self.rundata.clawdata.num_output_times = 1
-        self.rundata.clawdata.tfinal = 0.5
+        # self.rundata.clawdata.num_output_times = 1
+        # self.rundata.clawdata.tfinal = 0.5
 
-        self.rundata.amrdata.flag2refine_tol = 0.05
+        # self.rundata.amrdata.flag2refine_tol = 0.05
 
-        self.rundata.gaugedata.gauges = []
-        self.rundata.gaugedata.gauges.append([0, -1.0, 0., 1e9])
-        self.rundata.gaugedata.gauges.append([1, -2.5, 0., 1e9])
-        self.rundata.gaugedata.gauges.append([2, -1.75, 0., 1e9])
+        # self.rundata.gaugedata.gauges = []
+        # self.rundata.gaugedata.gauges.append([0, -1.0, 0., 1e9])
+        # self.rundata.gaugedata.gauges.append([1, -2.5, 0., 1e9])
+        # self.rundata.gaugedata.gauges.append([2, -1.75, 0., 1e9])
 
         # Look for adjoint data
         self.rundata.adjointdata.adjoint_outdir = adjoint_output
@@ -58,7 +58,7 @@ class Acoustics1DAdjointTest(test.AMRClawRegressionTest):
         # Perform tests
         self.check_gauges(save=save, gauge_id=0)
         self.check_gauges(save=save, gauge_id=1)
-        self.check_gauges(save=save, gauge_id=2)
+        # self.check_gauges(save=save, gauge_id=2)
 
         self.success = True
 
@@ -68,7 +68,7 @@ if __name__=="__main__":
     if len(sys.argv) > 1:
         if bool(sys.argv[1]):
             # Fake the setup and save out output
-            test = Acoustics1DAdjointTest()
+            test = Acoustics1DAdjointForwardTest()
             try:
                 test.setUp()
                 test.runTest(save=True)
