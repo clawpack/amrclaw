125c9f0b51817c26114aad237f1db21b76a34392
Kyle Mandli <kyle.mandli@gmail.com>
2025-09-12 10:49:22 -0400

    Switch acoustics 1d test

--- examples/acoustics_1d_homogeneous/test_acoustics_1d_homogeneous.py
+++ examples/acoustics_1d_homogeneous/test_acoustics_1d_homogeneous.py
@@ -28,44 +28,5 @@ def test_acoustics_1d_homogeneous(tmp_path: Path, save: bool):
     ctr.check_gauge(gauge_id=1)
 
 
-# class Acoustics1DTest_Homogeneous(test.AMRClawRegressionTest):
-#     """Basic test for a 1D acoustics test case in a homogeneous medium"""
-
-
-#     def runTest(self, save=False):
-
-#         # Write out data files
-#         self.load_rundata()
-        
-#         self.rundata.clawdata.num_output_times = 1
-#         self.rundata.clawdata.tfinal = 0.200000
-
-#         self.rundata.gaugedata.gauges = []
-#         self.rundata.gaugedata.gauges.append([0, 0.0, 0., 0.8])
-#         self.rundata.gaugedata.gauges.append([1, 0.6, 0., 0.8])
-
-#         self.write_rundata_objects()
-
-#         # Run code
-#         self.run_code()
-
-#         # Perform tests
-#         self.check_gauges(save=save, gauge_id=0)
-#         self.check_gauges(save=save, gauge_id=1)
-
-#         self.success = True
-
-
-
-# if __name__=="__main__":
-#     if len(sys.argv) > 1:
-#         if bool(sys.argv[1]):
-#             # Fake the setup and save out output
-#             test = Acoustics1DTest_Homogeneous()
-#             try:
-#                 test.setUp()
-#                 test.runTest(save=True)
-#             finally:
-#                 test.tearDown()
-#             sys.exit(0)
-#     unittest.main()
+if __name__ == "__main__":
+    pytest.main([__file__])
